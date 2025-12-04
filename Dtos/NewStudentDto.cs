c6424f53e89739e7c8d93515293f3519fdf1fb6d
unknown <abdulqauyumayomiposi652@gmail.com>
2025-12-04 16:16:34 +0100

    Initial

--- Dtos/NewStudentDto.cs
+++ Dtos/NewStudentDto.cs
@@ -5,15 +5,15 @@ using System.Threading.Tasks;
 
 namespace StudentMgtApp.Dtos
 {
-    public class StudentDto
+    public class NewStudentDto
     {
-        public int Id{ get; set; }
+         public int Id{ get; set; }
          public int GuardianId{ get; set; }
         public string Name{ get; set; } = default!;
         public int Age{get; set; }
         public string Class{ get; set; } = default!;
 
-        public record RegisterStudentRequest(string Name, string Class);
-        public record RegisterStudentResponse(int Id, string Name,int age,string GuardianName, string Class);
+        public record RegisterStudentRequest(string Name, string Class, int age);
+        public record RegisterStudentResponse(int Id, string Name);
     }
 }

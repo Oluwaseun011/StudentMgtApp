1d5c60e661b5890457373e729e55c1c04e0ace58
A Oluwaseun Idowu <oluwaseunidowu011@gmail.com>
2025-12-04 15:08:31 +0100

    seun

---
+++ Services/Interfaces/IGuardianRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudMgtApp.Services.Interfaces
+{
+    public interface IGuardianRepository
+    {
+        int GenerateId();
+        void AddToDb(Guardian guardian);
+        Guardian? GetGuardian(int id);
+        ICollection<Guardian> GetGuardians();
+
+    }
+}

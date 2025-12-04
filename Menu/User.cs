0cd8d1ea13c4470362f065e05f53e8db407d783e
unknown <kawtharkehinde@gmail.com>
2025-12-04 15:00:26 +0100

    initial

--- Menu/User.cs
+++ Menu/User.cs
@@ -11,5 +11,13 @@ namespace StudentMgtApp.Menu
         public string Email { get; set; } = default!;
         public string Password { get; set; } = default!;
         public string Role { get; set; } = default!;
+
+        public User(int id, string email, string password, string role)
+        {
+            Id = id;
+            Email = email;
+            Password = password;
+            Role = role;
+        }
     }
 }

05a9cdb49f8462d4f24f45c80d69102e34b97af4
sultaan0x <sultaanjamiuazeez@gmail.com>
2025-12-04 15:30:19 +0100

    initial

--- Context/a.cs
+++ Context/a.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 
 namespace StudMgtApp.Context
 {
-    public class a
+    public class Context
     {
-        
+        public static List<Guardian> guardians = new List<Guardian>();
+        public static List<User> users = new List<User>();
+        public static List<Student> students = new List<Student>();
     }
 }

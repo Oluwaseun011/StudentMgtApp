bc0a382f478fb28a79bda17912aac62db41f896a
A Oluwaseun Idowu <oluwaseunidowu011@gmail.com>
2025-12-04 15:54:20 +0100

    seun

---
+++ Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StudentMgtApp.Models.Entities;
+using StudentMgtApp.Repositories.Interfaces;
+using StudMgtApp.Context;
+
+namespace StudMgtApp.Repositories.Implementations
+{
+    public class UserRepository : IUserRepository
+    {
+        public void Add(User user)
+        {
+            StudContext.users.Add(user);
+        }
+
+        public User? GetByEmail(string email)
+        {
+            foreach (var item in StudContext.users)
+            {
+                if (item.Email == email)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        public User? GetById(int id)
+        {
+            return StudContext.users.Find(a => a.Id == id);
+        }
+
+        public int GetNextId()
+        {
+            return StudContext.users.Count + 1;
+        }
+
+        public List<User> GetUsers()
+        {
+            return StudContext.users;
+        }
+    }
+}

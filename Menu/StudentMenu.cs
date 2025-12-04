9f71cda9b13be0a83f6f379110df19a926b09cd3
sultaan0x <sultaanjamiuazeez@gmail.com>
2025-12-04 15:58:28 +0100

    What's up, Im done!

---
+++ Menu/StudentMenu.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentMgtApp.Menu
+{
+    public class StudentMenu
+    {
+        Console.WriteLine("Enter name of student ==>");
+        string name = Console.ReadLine() ??"";
+
+        Console.WriteLine("Enter age of student ==>");
+        int age = int.Parse(Console.ReadLine() ??"");
+
+        Console.WriteLine("Enter class of student ==>");
+        int studClass = int.Parse(Console.ReadLine() ??"");
+
+       
+    }
+}
